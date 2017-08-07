1eec59b90be5c2ae390875b0a8966c2623a094e6
ruko <rukokarasu@gmail.com>
2017-08-07 16:53:00 -0700

    Fix some more mimikyu stuff

--- data/base_stats/mimikyu.asm
+++ data/base_stats/mimikyu.asm
@@ -7,15 +7,15 @@
 	db 45 ; catch rate
 	db 167 ; base exp
 	db NO_ITEM ; item 1
-	db LUCKY_EGG ; item 2
+	db NO_ITEM ; item 2
 	db 127 ; gender
 	db 100 ; unknown
-	db 40 ; step cycles to hatch
+	db 20 ; step cycles to hatch
 	db 5 ; unknown
-	INCBIN "gfx/pics/blissey/front.dimensions"
+	INCBIN "gfx/pics/mimikyu/front.dimensions"
 	db 0, 0, 0, 0 ; padding
 	db FAST ; growth rate
-	dn FAIRY, FAIRY ; egg groups
+	dn AMORPHOUS, AMORPHOUS ; egg groups
 
 	; tmhm
 	tmhm HEADBUTT, CURSE, ROAR, TOXIC, ROCK_SMASH, HIDDEN_POWER, SUNNY_DAY, SNORE, BLIZZARD, HYPER_BEAM, ICY_WIND, PROTECT, RAIN_DANCE, ENDURE, FRUSTRATION, SOLARBEAM, IRON_TAIL, RETURN, DIG, SHADOW_BALL, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SLUDGE_BOMB, FIRE_BLAST, DEFENSE_CURL, DREAM_EATER, REST, ATTRACT, FURY_CUTTER, CUT, FLASH
